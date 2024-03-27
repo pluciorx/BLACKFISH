5ea363b73a22a5d3dc733cb38a8ee32765b23465
Marcin Pluta <pluciorx@gmail.com>
2024-03-27 17:52:29 +0100

    Rename solution- Add production line stub

---
+++ SmallProductionLine/SmallProductionLine.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SmallProductionLine.ino
+ Created:	3/27/2024 5:51:58 PM
+ Author:	pluci
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
